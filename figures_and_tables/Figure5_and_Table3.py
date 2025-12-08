ed41873bce5095d5c15e9f613cb30dc51cd8e11a
afrakilic <hafra.kilic@gmail.com>
2025-12-08 22:49:02 +0100

    wip

--- figures_and_tables/Figure5_and_Table3.py
+++ figures_and_tables/Figure5_and_Table3.py
@@ -224,5 +224,5 @@ fig.legend(
 )
 
 plt.tight_layout(rect=[0, 0, 0.88, 1])
-# plt.savefig("plot3.pdf", format='pdf', bbox_inches='tight')
+plt.savefig("plot3.pdf", format='pdf', bbox_inches='tight')
 plt.show()
